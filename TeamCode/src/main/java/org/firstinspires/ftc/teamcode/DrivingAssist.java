ff9f98d806b9eb6fa3c954bd22a60a82d3b41009
angrybirdsFTC <151655500+angrybirdsFTC@users.noreply.github.com>
2024-03-10 09:23:22 -0700

    Removed debug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivingAssist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivingAssist.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 public class DrivingAssist {
-    //final int MAX_TICKS = 100;
+    final int MAX_TICKS = 100;
 
     DigitalChannel leftSwitch;
     DigitalChannel rightSwitch;
@@ -13,16 +13,16 @@ public class DrivingAssist {
     DigitalChannel led2;
 
     Controller controller;
-    DetectPixel detectPixel;
 
-    //int ticksRemaining = 0;
+
+    int ticksRemaining = 0;
     //double tempSpeed = 0;
     boolean prevLeftSwitchState = false;
     boolean prevRightSwitchState = false;
 
-    public DrivingAssist(Controller controller, HardwareMap hardwareMap, DetectPixel detectPixel) {
+    public DrivingAssist(Controller controller, HardwareMap hardwareMap) {
         this.controller = controller;
-        this.detectPixel = detectPixel;
+
 
         leftSwitch = hardwareMap.get(DigitalChannel.class, "leftSwitch");
         rightSwitch = hardwareMap.get(DigitalChannel.class, "rightSwitch");
@@ -35,9 +35,6 @@ public class DrivingAssist {
     public void gripLed(Gamepad gamepad1, Gamepad gamepad2) {
         led1.setState(!leftSwitch.getState());
         led2.setState(!rightSwitch.getState());
-
-        controller.Debug("left switch", !leftSwitch.getState());
-        controller.Debug("right switch", !rightSwitch.getState());
 
         // Check if previous state is false and current state is true
         if (prevLeftSwitchState && !leftSwitch.getState()) {
@@ -53,26 +50,16 @@ public class DrivingAssist {
         prevRightSwitchState = rightSwitch.getState();
     }
 
-//    public void rumble(Gamepad gamepad1, Gamepad gamepad2) {
-//        //rumbleSpeed(gamepad1, gamepad2, 0.1);
-//        //detectPixel.telemetryTfod();
-//        //controller.Debug("test y", detectPixel.getDistance());
-//
-////        tempSpeed -= gamepad2.right_stick_y / 1000;
-////        RumbleSpeed(gamepad1, gamepad2, tempSpeed);
-////        controller.Debug("temp speed", tempSpeed);
-//    }
+    void rumbleSpeed(Gamepad gamepad1, Gamepad gamepad2, double speed) {
+        if (speed > 1 || speed < 0) return;
 
-//    void rumbleSpeed(Gamepad gamepad1, Gamepad gamepad2, double speed) {
-//        if (speed > 1 || speed < 0) return;
-//
-//        if (ticksRemaining == 0) {
-//            gamepad1.rumble(100);
-//            gamepad2.rumble(100);
-//            ticksRemaining = MAX_TICKS - (int)(speed * MAX_TICKS);
-//        }
-//        else {
-//            ticksRemaining--;
-//        }
-//    }
+        if (ticksRemaining == 0) {
+            gamepad1.rumble(100);
+            gamepad2.rumble(100);
+            ticksRemaining = MAX_TICKS - (int)(speed * MAX_TICKS);
+        }
+        else {
+            ticksRemaining--;
+        }
+    }
 }
