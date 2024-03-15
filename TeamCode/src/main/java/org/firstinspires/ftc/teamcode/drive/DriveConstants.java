8586d6468cd78e1bfb1b763291164346a23e7c5e
angrybirdsFTC <angrybirdsftc@gmail.com>
2024-03-15 14:44:10 +0200

    Updated Kv, Ka and KStatic. also velocities.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -24,7 +24,7 @@ public class DriveConstants {
      * These are motor constants that should be listed online for your motors.
      */
     public static final double TICKS_PER_REV = 384.5;
-    public static final double MAX_RPM = 435;
+    public static final double MAX_RPM = 312;
 
     /*
      * Set RUN_USING_ENCODER to true to enable built-in hub velocity control using drive encoders.
@@ -47,7 +47,7 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 1.88976; // in
-    public static double GEAR_RATIO = 19.2; // output (wheel) speed / input (motor) speed
+    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed - 19.2
     public static double TRACK_WIDTH = 16.34; // in
 
     /*
@@ -56,9 +56,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1/rpmToVelocity(MAX_RPM);
-    public static double kA = 0.00199;
-    public static double kStatic = 0.01597;
+    public static double kV = 0.0113;
+    public static double kA = 0.00016;
+    public static double kStatic = 0.085;
 
     public static RevHubOrientationOnRobot.LogoFacingDirection LOGO_FACING_DIR = RevHubOrientationOnRobot.LogoFacingDirection.UP;
 
@@ -92,10 +92,10 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
 
      */
-    public static double MAX_VEL = (MAX_RPM/60) * GEAR_RATIO * WHEEL_RADIUS * 2 * Math.PI;
-    public static double MAX_ACCEL = 52.48180821614297;
-    public static double MAX_ANG_VEL = Math.toRadians(184.02607784577722);
-    public static double MAX_ANG_ACCEL = Math.toRadians(184.02607784577722);
+    public static double MAX_VEL = 37.74330378369762;
+    public static double MAX_ACCEL = 62.48180821614297;
+    public static double MAX_ANG_VEL = 3.211860967940206;
+    public static double MAX_ANG_ACCEL = 3.211860967940206;
 
 
     public static double encoderTicksToInches(double ticks) {
