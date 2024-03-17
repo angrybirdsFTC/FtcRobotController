96f4ab37886aa47033af620e1ada47118de20001
AmiT1458 <97545597+AmiT1458@users.noreply.github.com>
2024-03-17 16:37:44 +0200

    Changed Roadrunenr custom test to autonomous opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RR_Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RR_Autonomous.java
@@ -4,20 +4,22 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.PoseStorage;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 /*
  * This is a simple routine to test translational drive capabilities.
  */
-@Config
-@Autonomous(group = "drive")
+
+@Autonomous(name = "TestRoadrunner", group = "SA_FTC")
 public class RR_Autonomous extends LinearOpMode {
-    public static double DISTANCE = 60; // in
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -25,21 +27,25 @@ public class RR_Autonomous extends LinearOpMode {
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        Trajectory trajectory = drive.trajectoryBuilder(new Pose2d())
-                .forward(DISTANCE)
+        Trajectory trajectory1 = drive.trajectoryBuilder(new Pose2d())
+                .splineTo(new Vector2d(40, 0), 0)
+                .splineTo(new Vector2d(40,25), Math.toRadians(90))
+                .splineToConstantHeading(new Vector2d(0,0), Math.toRadians(90)) // go to the starting pos of this Autonomous (NOT the 0,0 of the field/the center of the field)
                 .build();
 
         waitForStart();
 
         if (isStopRequested()) return;
 
-        drive.followTrajectory(trajectory);
+        drive.followTrajectory(trajectory1);
 
         Pose2d poseEstimate = drive.getPoseEstimate();
         telemetry.addData("finalX", poseEstimate.getX());
         telemetry.addData("finalY", poseEstimate.getY());
         telemetry.addData("finalHeading", poseEstimate.getHeading());
         telemetry.update();
+
+        PoseStorage.currentPose = drive.getPoseEstimate();
 
         while (!isStopRequested() && opModeIsActive()) ;
     }
